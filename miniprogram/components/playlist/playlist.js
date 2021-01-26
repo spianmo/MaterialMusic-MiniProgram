21b36312699f4a96061780ab3ffe18bf4479456c
Finger <Finger@spianmo.com>
2021-01-26 15:22:13 +0800

    自定义歌单组件，设计推荐导航栏

---
+++ miniprogram/components/playlist/playlist.js
@@ -0,0 +1,26 @@
+// components/playlist/playlist.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    playlist:{
+      type: Array,
+      value:[]
+    }
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
